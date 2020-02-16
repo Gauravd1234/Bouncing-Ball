364550c0d5df7987908e52736bb0bff30fa8ba63
Gauravd1234 <52632861+Gauravd1234@users.noreply.github.com>
2020-02-16 15:12:05 -0800

    Create Bouncing_Ball.js

---
+++ Bouncing_Ball.js
@@ -0,0 +1,27 @@
+
+var xval = 400;
+var speed = 0;
+
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(0);
+  fill(255, 0, 0);
+  ellipse(200, xval, 25);
+  if (xval > width - 100){   
+    speed -= 1;   
+  }else{
+    
+  speed += 0.5;
+    
+  }
+  
+  
+  xval += speed;
+  
+  
+}
+
+
